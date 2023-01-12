d6d5fb7457f89a0f847274b107706055f53cc2ea
Chester Curme <chestercurme@microsoft.com>
2023-01-12 03:35:41 -0500

    add tests

--- llpandas/llm_accessor.py
+++ llpandas/llm_accessor.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pandas as pd
 
 from llpandas.chains import LLM_CHAIN
@@ -9,13 +11,20 @@ class LLMAccessor:
     def __init__(self, pandas_df: pd.DataFrame):
         self.df = pandas_df
 
-    def query(self, query):
+    def query(self, query: str, verify: bool = True) -> Any:
         df = self.df
         inputs = {"objective": query, "df_head": df.head(), "stop": "```"}
         llm_response = LLM_CHAIN.run(**inputs)
-        print("suggested code:")
-        print(llm_response)
-        print("run this code? y/n")
-        user_input = input()
-        if user_input == "y":
+        eval_expression = False
+        if verify:
+            print("suggested code:")
+            print(llm_response)
+            print("run this code? y/n")
+            user_input = input()
+            if user_input == "y":
+                eval_expression = True
+        else:
+            eval_expression = True
+
+        if eval_expression:
             return(eval(llm_response))
---
+++ tests/test_llm_accessor.py
@@ -0,0 +1,30 @@
+import unittest
+
+from llpandas.llm_accessor import pd
+
+
+class TestLLMAccessor(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.product_df = pd.DataFrame(
+            [
+                {"name": "The Da Vinci Code", "type": "book", "price": 15, "quantity": 300, "rating": 4},
+                {"name": "Jurassic Park", "type": "book", "price": 12, "quantity": 400, "rating": 4.5},
+                {"name": "Jurassic Park", "type": "film", "price": 8, "quantity": 6, "rating": 5},
+                {"name": "Matilda", "type": "book", "price": 5, "quantity": 80, "rating": 4},
+            ],
+        )
+    
+    def test_basic_use(self):
+        result = self.product_df.llm.query(
+            "What is the price of the highest-priced book?",
+            verify=False,
+        )
+        expected_result = 15
+        self.assertEqual(expected_result, result)
+
+    def test_sliced(self):
+        result = self.product_df[["name", "type", "price", "rating"]].llm.query("What is the price of the highest-priced book?", verify=False)
+        expected_result = 15
+        self.assertEqual(expected_result, result)
