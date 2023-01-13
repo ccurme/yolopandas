07ef370e4290c33931a6fe83e415753981cb2b92
Chester Curme <chestercurme@microsoft.com>
2023-01-12 22:48:11 -0500

    accommodate multiple lines

--- llpandas/llm_accessor.py
+++ llpandas/llm_accessor.py
@@ -1,3 +1,4 @@
+import ast
 from typing import Any
 
 import pandas as pd
@@ -11,6 +12,7 @@ class LLMAccessor:
         self.df = pandas_df
 
     def query(self, query: str, verify: bool = True) -> Any:
+        """Query the dataframe with natural language."""
         df = self.df
         inputs = {"objective": query, "df_head": df.head(), "stop": "```"}
         llm_response = LLM_CHAIN.run(**inputs)
@@ -26,4 +28,10 @@ class LLMAccessor:
             eval_expression = True
 
         if eval_expression:
-            return eval(llm_response)
+            # WARNING: This is a bad idea. Here we evaluate the (potentially multi-line)
+            # llm response. Do not use unless you trust that llm_response is not malicious.
+            tree = ast.parse(llm_response)
+            module = ast.Module(tree.body[:-1], type_ignores=[])
+            expression = ast.Expression(tree.body[-1].value)
+            exec(compile(module, "", "exec"))
+            return eval(compile(expression, "", "eval"))
--- tests/test_llm_accessor.py
+++ tests/test_llm_accessor.py
@@ -32,7 +32,7 @@ class TestLLMAccessor(unittest.TestCase):
                 {
                     "name": "Matilda",
                     "type": "book",
-                    "price": 5,
+                    "price": 6,
                     "quantity": 80,
                     "rating": 4,
                 },
@@ -47,9 +47,39 @@ class TestLLMAccessor(unittest.TestCase):
         expected_result = 15
         self.assertEqual(expected_result, result)
 
+        result = self.product_df.llm.query(
+            "What is the average price of products grouped by type?",
+            verify=False,
+        )
+        expected = self.product_df.groupby("type")["price"].mean()
+        pd.testing.assert_series_equal(expected, result)
+
+        result = self.product_df.llm.query(
+            "Give me products that are not books.",
+            verify=False,
+        )
+        expected = self.product_df[self.product_df["type"] != "book"]
+        pd.testing.assert_frame_equal(expected, result)
+
     def test_sliced(self):
         result = self.product_df[["name", "type", "price", "rating"]].llm.query(
             "What is the price of the highest-priced book?", verify=False
         )
         expected_result = 15
         self.assertEqual(expected_result, result)
+
+    def test_multi_line(self):
+        query = """
+        Add a column `new_column` to the dataframe which is range 1 - number of rows,
+        then return the mean of this column by each type.
+        """
+        # Here we test that we can accommodate multiple lines. e.g., here we will have:
+        # df['new_column'] = range(1, len(df) + 1)
+        # df.groupby('type')['new_column'].mean()
+        result = self.product_df.llm.query(query, verify=False)
+        expected = (
+            self.product_df.assign(new_column=range(1, len(self.product_df) + 1))
+            .groupby("type")["new_column"]
+            .mean()
+        )
+        pd.testing.assert_series_equal(expected, result)
