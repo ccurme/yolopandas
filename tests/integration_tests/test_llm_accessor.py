4b66896ee2ab94624140eaacf1ecec9a48e60249
Chester Curme <chestercurme@microsoft.com>
2023-01-14 21:18:58 -0500

    split out unit tests

--- tests/integration_tests/test_llm_accessor.py
+++ tests/integration_tests/test_llm_accessor.py
@@ -34,41 +34,6 @@ class TestLLMAccessor(unittest.TestCase):
         expected = self.product_df[self.product_df["type"] != "book"]
         pd.testing.assert_frame_equal(expected, result)
 
-    def test_sliced(self):
-        result = self.product_df[["name", "type", "price", "rating"]].llm.query(
-            "What is the price of the highest-priced book?", verify=False
-        )
-        expected_result = 15
-        self.assertEqual(expected_result, result)
-
-    def test_multi_line(self):
-        query = """
-        Add a column `new_column` to the dataframe which is range 1 - number of rows,
-        then return the mean of this column by each type.
-        """
-        # Here we test that we can accommodate multiple lines. e.g., here we will have:
-        # df['new_column'] = range(1, len(df) + 1)
-        # df.groupby('type')['new_column'].mean()
-        result = self.product_df.llm.query(query, verify=False)
-        expected = (
-            self.product_df.assign(new_column=range(1, len(self.product_df) + 1))
-            .groupby("type")["new_column"]
-            .mean()
-        )
-        pd.testing.assert_series_equal(expected, result)
-
-    def test_multiline_exec(self):
-        """Test a multiline command when the final line should be exec'd not eval'd."""
-        query = """
-        Add a column `new_column` to the dataframe which is range 1 - number of rows,
-        then add a column `foo` which is always the value 1
-        """
-        self.product_df.llm.query(query, verify=False)
-        expected_df = self.product_df.assign(
-            new_column=range(1, len(self.product_df) + 1)
-        ).assign(foo=1)
-        pd.testing.assert_frame_equal(expected_df, self.product_df)
-
     def test_memory(self):
         self.product_df.llm.reset_chain(use_memory=True)
         _ = self.product_df.llm.query(
--- tests/unit_tests/test_llm_accessor.py
+++ tests/unit_tests/test_llm_accessor.py
@@ -3,7 +3,6 @@ import unittest
 from unittest.mock import Mock, patch
 
 from llpandas.llm_accessor import pd
-import llpandas.llm_accessor
 from langchain.chains.base import Chain
 from tests import TEST_DIRECTORY
 
@@ -31,3 +30,71 @@ class TestLLMAccessor(unittest.TestCase):
         )
         expected_result = 15
         self.assertEqual(expected_result, result)
+
+        mock.return_value = _get_mock_chain("df.groupby('type')['price'].mean()")
+        self.product_df.llm.reset_chain()
+        result = self.product_df.llm.query(
+            "What is the average price of products grouped by type?",
+            verify=False,
+        )
+        expected_result = self.product_df.groupby('type')['price'].mean()
+        pd.testing.assert_series_equal(expected_result, result)
+
+        mock.return_value = _get_mock_chain("df[df['type'] != 'book']")
+        self.product_df.llm.reset_chain()
+        result = self.product_df.llm.query(
+            "Give me products that are not books.",
+            verify=False,
+        )
+        expected = self.product_df[self.product_df["type"] != "book"]
+        pd.testing.assert_frame_equal(expected, result)
+
+    @patch('llpandas.llm_accessor.get_chain')
+    def test_sliced(self, mock):
+        mock.return_value = _get_mock_chain("df[df['type'] == 'book']['price'].max()")
+        self.product_df.llm.reset_chain()
+        result = self.product_df[["name", "type", "price", "rating"]].llm.query(
+            "What is the price of the highest-priced book?", verify=False
+        )
+        expected_result = 15
+        self.assertEqual(expected_result, result)
+
+    @patch('llpandas.llm_accessor.get_chain')
+    def test_multi_line(self, mock):
+        """Test that we can accommodate multiple lines in the LLM response."""
+        query = """
+        Add a column `new_column` to the dataframe which is range 1 - number of rows,
+        then return the mean of this column by each type.
+        """
+        mock_response = (
+            "df['new_column'] = range(1, len(df) + 1)\n"
+            "df.groupby('type')['new_column'].mean()"
+        )
+        mock.return_value = _get_mock_chain(mock_response)
+        self.product_df.llm.reset_chain()
+        result = self.product_df.llm.query(query, verify=False)
+        expected = (
+            self.product_df.assign(new_column=range(1, len(self.product_df) + 1))
+            .groupby("type")["new_column"]
+            .mean()
+        )
+        pd.testing.assert_series_equal(expected, result)
+
+    @patch('llpandas.llm_accessor.get_chain')
+    def test_multiline_exec(self, mock):
+        """Test a multiline command when the final line should be exec'd not eval'd."""
+        query = """
+        Add a column `new_column` to the dataframe which is range 1 - number of rows,
+        then add a column `foo` which is always the value 1
+        """
+        mock_response = (
+            "df['new_column'] = range(1, len(df) + 1)\n"
+            "df['foo'] = 1"
+        )
+        mock.return_value = _get_mock_chain(mock_response)
+        self.product_df.llm.reset_chain()
+        self.product_df.llm.query(query, verify=False)
+        expected_df = self.product_df.assign(
+            new_column=range(1, len(self.product_df) + 1)
+        ).assign(foo=1)
+        pd.testing.assert_frame_equal(expected_df, self.product_df)
